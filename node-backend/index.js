f5dfd853fa48e0ca1565936ef4dbbc94f01d5f0f
Akaronte <akaronte@gmail.com>
2020-02-09 16:41:26 +0100

    initial commit

---
+++ node-backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+const fs = require('fs')
+
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post('/uploadfile', async (req, res) => {
+    //console.log(req.files)
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+
+            let file = req.files.file;
+            
+
+            file.mv('./uploads/' + file.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: file.name,
+                    mimetype: file.mimetype,
+                    size: file.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.use(morgan('dev'));
+
+//start app 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);
---
+++ vue-frontend/src/components/Dropzone.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <vue-dropzone
+      ref="dropzone"
+      id="drop1"
+      :options="dropOptions"
+      @vdropzone-complete="afterComplete"
+    ></vue-dropzone>
+    <button @click="sendFiles">Send Files</button>
+  </div>
+</template>
+
+<script>
+import vueDropzone from "vue2-dropzone";
+
+export default {
+  data: () => ({
+    dropOptions: {
+      url: "http://localhost:3000/uploadfile",
+    //   maxFilesize: 2, // MB
+    //   maxFiles: 4,
+    //   chunking: true,
+    //   chunkSize: 500, // Bytes
+    //   thumbnailWidth: 150, // px
+    //   thumbnailHeight: 150,
+      addRemoveLinks: true,
+      autoProcessQueue: false
+    }
+  }),
+    methods: {
+    
+    afterComplete(file) {
+      console.log(file);
+    },
+    removeAllFiles() {
+        this.$refs.dropzone.removeAllFiles(true);
+    },
+    sendFiles(){
+        console.log('Sendfiles');
+        this.$refs.dropzone.processQueue();
+        console.log('RemoveFiles');
+        this.removeAllFiles();
+    }
+  },
+  components: {
+    vueDropzone
+  }
+};
+</script>
